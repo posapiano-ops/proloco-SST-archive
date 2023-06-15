b1aca820024d73c2de41abbe03423d5658a32252
posapiano-ops <rfiorito@outlook.it>
2023-06-15 16:16:39 +0200

    rename

--- www.prolocosansalvatoretelesino.it/wp-content/plugins/nextgen-gallery/xml/media-rss.php
+++ www.prolocosansalvatoretelesino.it/wp-content/plugins/nextgen-gallery/xml/media-rss.php
@@ -4,117 +4,117 @@
 		<generator><![CDATA[NextGEN Gallery [http://alexrabe.boelinger.com]]]></generator>
 		<title>pro Loco San Salvatore Telesino</title>
 		<description>cultura, agroalimentare, ambiente e turismo</description>
-		<link><![CDATA[http://www.prolocosansalvatoretelesino.it]]></link>
-		<atom:link rel='next' href='http://www.prolocosansalvatoretelesino.it/wp-content/plugins/nextgen-gallery/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<link><![CDATA[index.html]]></link>
+		<atom:link rel='next' href='./wp-content/plugins/nextgen-gallery/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
 		<item>
 			<title><![CDATA[la-rocca_4]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_4.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_4.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/cultura/la-rocca_4.jpg]]></link>
+			<media:content url='./wp-content/gallery/cultura/la-rocca_4.jpg' medium='image' />
 			<media:title><![CDATA[la-rocca_4]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/thumbs/thumbs_la-rocca_4.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/cultura/thumbs/thumbs_la-rocca_4.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[la-rocca_3]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_3.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_3.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/cultura/la-rocca_3.jpg]]></link>
+			<media:content url='./wp-content/gallery/cultura/la-rocca_3.jpg' medium='image' />
 			<media:title><![CDATA[la-rocca_3]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/thumbs/thumbs_la-rocca_3.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/cultura/thumbs/thumbs_la-rocca_3.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[la-rocca_2]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_2.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_2.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/cultura/la-rocca_2.jpg]]></link>
+			<media:content url='./wp-content/gallery/cultura/la-rocca_2.jpg' medium='image' />
 			<media:title><![CDATA[la-rocca_2]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/thumbs/thumbs_la-rocca_2.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/cultura/thumbs/thumbs_la-rocca_2.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[la-rocca_1]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_1.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca_1.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/cultura/la-rocca_1.jpg]]></link>
+			<media:content url='./wp-content/gallery/cultura/la-rocca_1.jpg' medium='image' />
 			<media:title><![CDATA[la-rocca_1]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/thumbs/thumbs_la-rocca_1.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/cultura/thumbs/thumbs_la-rocca_1.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[la-rocca]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/la-rocca.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/cultura/la-rocca.jpg]]></link>
+			<media:content url='./wp-content/gallery/cultura/la-rocca.jpg' medium='image' />
 			<media:title><![CDATA[la-rocca]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/cultura/thumbs/thumbs_la-rocca.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/cultura/thumbs/thumbs_la-rocca.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[struppoli]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/struppoli_2.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/struppoli_2.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/agroalimentare/struppoli_2.jpg]]></link>
+			<media:content url='./wp-content/gallery/agroalimentare/struppoli_2.jpg' medium='image' />
 			<media:title><![CDATA[struppoli]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/thumbs/thumbs_struppoli_2.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/agroalimentare/thumbs/thumbs_struppoli_2.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[prodotti tipici, agroalimentare, tipicità, struppolo, mela annurca, gastronomia]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[struppoli]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/struppoli_1.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/struppoli_1.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/agroalimentare/struppoli_1.jpg]]></link>
+			<media:content url='./wp-content/gallery/agroalimentare/struppoli_1.jpg' medium='image' />
 			<media:title><![CDATA[struppoli]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/thumbs/thumbs_struppoli_1.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/agroalimentare/thumbs/thumbs_struppoli_1.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[prodotti tipici, agroalimentare, tipicità, struppolo, mela annurca, gastronomia]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[struppoli]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/struppoli.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/struppoli.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/agroalimentare/struppoli.jpg]]></link>
+			<media:content url='./wp-content/gallery/agroalimentare/struppoli.jpg' medium='image' />
 			<media:title><![CDATA[struppoli]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/thumbs/thumbs_struppoli.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/agroalimentare/thumbs/thumbs_struppoli.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[prodotti tipici, agroalimentare, tipicità, struppolo, mela annurca, gastronomia]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[prodotti tipici]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/caciocavallo.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/caciocavallo.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/agroalimentare/caciocavallo.jpg]]></link>
+			<media:content url='./wp-content/gallery/agroalimentare/caciocavallo.jpg' medium='image' />
 			<media:title><![CDATA[prodotti tipici]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/thumbs/thumbs_caciocavallo.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/agroalimentare/thumbs/thumbs_caciocavallo.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[prodotti tipici, agroalimentare, tipicità, struppolo, mela annurca, gastronomia]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 		<item>
 			<title><![CDATA[melannurca]]></title>
 			<description><![CDATA[]]></description>
-			<link><![CDATA[http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/annurca_4.jpg]]></link>
-			<media:content url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/annurca_4.jpg' medium='image' />
+			<link><![CDATA[./wp-content/gallery/agroalimentare/annurca_4.jpg]]></link>
+			<media:content url='./wp-content/gallery/agroalimentare/annurca_4.jpg' medium='image' />
 			<media:title><![CDATA[melannurca]]></media:title>
 			<media:description><![CDATA[]]></media:description>
-			<media:thumbnail url='http://www.prolocosansalvatoretelesino.it/wp-content/gallery/agroalimentare/thumbs/thumbs_annurca_4.jpg' width='100' height='75' />
+			<media:thumbnail url='./wp-content/gallery/agroalimentare/thumbs/thumbs_annurca_4.jpg' width='100' height='75' />
 			<media:keywords><![CDATA[prodotti tipici, agroalimentare, tipicità, struppolo, mela annurca, gastronomia]]></media:keywords>
-			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (http://www.prolocosansalvatoretelesino.it)]]></media:copyright>
+			<media:copyright><![CDATA[Copyright (c) pro Loco San Salvatore Telesino (index.html)]]></media:copyright>
 		</item>
 	</channel>
 </rss>
