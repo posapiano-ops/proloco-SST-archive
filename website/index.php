e458a40aa9bcbdf128b96795ac8b870df46c02a1
posapiano-ops <rfiorito@outlook.it>
2023-06-16 17:02:03 +0200

    minor fix + menu

--- website/index.php
+++ website/index.php
@@ -5,16 +5,14 @@ $p="index";
 if (isset($_GET["page"])){
     $p=$_GET["page"];
     if ($p!=basename($p) || $p=="index" || !file_exists($p.".html") || !preg_match("/^[A-Za-z0-9-_]+$/",$p)){
-        $p="index"; //dovrebbe essere una pagina di errore ma non è!!!!
+        $p="error"; //dovrebbe essere una pagina di errore ma non è!!!!
     }
-    Template::view($p.'.html');
-}else{
-    Template::view($p.'.html');
 }
+Template::view($p.'.html');
+Template::clearCache();
 
 #Template::view('about.html', [
 #    'title' => 'Home Page',
 #    'colors' => ['red','blue','green']
 #]);
 
-#Template::clearCache();
