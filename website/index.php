a3a0d9c31a1449f4db4d309e1323a7912f56f72b
posapiano-ops <rfiorito@outlook.it>
2023-06-17 17:11:45 +0200

    rebase php template "page_id"

--- website/index.php
+++ website/index.php
@@ -2,8 +2,8 @@
 include 'template.php';
 $p="index";
 
-if (isset($_GET["page"])){
-    $p=$_GET["page"];
+if (isset($_GET["page_id"])){
+    $p=$_GET["page_id"];
     if ($p!=basename($p) || $p=="index" || !file_exists($p.".html") || !preg_match("/^[A-Za-z0-9-_]+$/",$p)){
         $p="error"; //dovrebbe essere una pagina di errore ma non è!!!!
     }
